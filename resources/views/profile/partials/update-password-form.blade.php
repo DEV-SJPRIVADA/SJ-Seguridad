cb2d775ec9b47ced617b489a82fff22ed0d2e85c
Manuel Espinosa <mawilespinosa@gmail.com>
2026-08-06 16:10:27 -0500

    Refactoriza el formulario de actualización de contraseña en la vista de perfil para utilizar componentes de entrada de contraseña personalizados. Se actualizan las pruebas para verificar la presencia de los nuevos campos de contraseña en la página de perfil.

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -15,19 +15,19 @@
 
         <div class="form-field">
             <x-input-label for="update_password_current_password" :value="__('Current Password')" />
-            <x-text-input id="update_password_current_password" name="current_password" type="password" autocomplete="current-password" />
+            <x-password-input id="update_password_current_password" name="current_password" autocomplete="current-password" />
             <x-input-error :messages="$errors->updatePassword->get('current_password')" />
         </div>
 
         <div class="form-field">
             <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input id="update_password_password" name="password" type="password" autocomplete="new-password" />
+            <x-password-input id="update_password_password" name="password" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password')" />
         </div>
 
         <div class="form-field">
             <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" autocomplete="new-password" />
+            <x-password-input id="update_password_password_confirmation" name="password_confirmation" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" />
         </div>
 
